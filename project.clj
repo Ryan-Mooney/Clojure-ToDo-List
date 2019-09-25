86a3f18135bbe012d198c66f90210e253ab9a4d3
Ryan Mooney <mooneyryanj@gmail.com>
2019-09-25 15:47:03 -0400

    Added heroku functionality

--- project.clj
+++ project.clj
@@ -6,10 +6,14 @@
   ;;When running locally, it is important to have the same version numbers next to each
   :dependencies [[org.clojure/clojure "1.10.1"]
                  ;;While this app uses ring, the asgard app uses http-kit to control their web connections
-                 [ring "1.7.1"]]
-  :repl-options {:init-ns todo-list.core}
+                 [ring "1.7.1"]
+                 [compojure "1.3.4"]]
   ;; :main setting below tells what to do when 'lein run' is ran
   :main todo-list.core
+    ;; Used to configure the leiningen build for heroku
+  :min-lein-version "2.0.0"
+  :uberjar-name "todo-list.jar"
+  :repl-options {:init-ns todo-list.core}
   ;; By adding a dev profile, the lein run command will look for this first during development.
   :profiles {:dev
              {:main todo-list.core/-dev-main}})
